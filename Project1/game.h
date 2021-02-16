ded06b58f0749e2cf58f40e472439f85c8ab3c45
Mori-Sama <3236708675@qq.com>
2021-02-16 22:52:33 +0800

    闲作

--- Project1/game.h
+++
@@ -1,9 +0,0 @@
-
-#define ROW 3
-#define COL 3
-void init_board(char board[ROW][COL],int row, int col);
-void show_board(char board[ROW][COL], int row, int col);
-void player_move(char board[ROW][COL], int row, int col);
-char judge_outcome(char board[ROW][COL], int row, int col);
-void player_move1(char board[ROW][COL], int row, int col);
-int is_win(char flag);
